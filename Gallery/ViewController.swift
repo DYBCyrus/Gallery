36bbaf29c9e589b9375c0c404850f5250eaac4ad
Jiaxiao Zhou <jiz417@ucsd.edu>
2017-10-20 22:48:47 -0700

    added project

--- Gallery/ViewController.swift
+++ Gallery/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
